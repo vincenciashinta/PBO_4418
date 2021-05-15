fbb317ce4ef22abc1aed1992e3fc2005e01f5123
PBO_4418 <vincenciashinta@gmail.com>
2021-05-15 16:16:20 +0700

    PBO_4418

--- PERTEMUAN 5/PRAKTEK/tesVehicle.java
+++
@@ -1,9 +0,0 @@
-public class tesVehicle{
-    public static void main(String[] args){
-        Bicycle testBicycle=new Bicycle();
-        testBicycle.color();
-        Motorcycle honda=new Motorcycle();
-        honda.get_speed(126);
-        
-    }
-}
